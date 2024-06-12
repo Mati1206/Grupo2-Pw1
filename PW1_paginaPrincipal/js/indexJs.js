ce85d85076b7c10ca42e33447ec711b01e275fd6
MateoColace <166334360+MateoColace@users.noreply.github.com>
2024-06-11 21:00:14 -0300

    avances de home

---
+++ PW1_paginaPrincipal/js/indexJs.js
@@ -0,0 +1,175 @@
+const PELICULAS_Y_SERIES = 
+[
+    {
+        título: "John Wick",
+        genero: "Acción",
+        sinopsis: "John Wick, un exasesino a sueldo, vive en paz tras la muerte de su esposa. Su tranquilidad se quiebra cuando el hijo de un mafioso roba su auto y mata a su cachorro, último regalo de su esposa. Esto lo lleva a retomar su antigua vida y buscar venganza contra quienes le arrebataron lo que más amaba.",
+        duración: "1h 41m",
+        poster: "./images/JohnWick.jpg"
+    },
+
+    {
+        título: "The Love Witch",
+        genero: "Thriller/Terror",
+        sinopsis: "Elaine, una joven y hermosa bruja, está obsesionada con encontrar el amor verdadero. Se muda a una pequeña ciudad de California donde utiliza magia y hechizos para atraer a los hombres. Sin embargo, sus encantos tienen consecuencias trágicas y mortales. A medida que sus conjuros causan estragos, Elaine se ve atrapada en un enredo de pasión, locura y muerte, cuestionando la naturaleza del amor y el poder de la seducción. La película es una mezcla estilizada de terror y comedia, con una estética que rinde homenaje a los filmes de los años 60 y 70.",
+        duración: "2h",
+        poster: "./images/TheLoveWithc.jpg"
+    },
+
+    {
+        título: "Frozen",
+        genero: "Familiar/Fantasía",
+        sinopsis: "En el reino de Arendelle, la joven princesa Elsa posee poderes mágicos que le permiten crear hielo y nieve. Tras un accidente durante su infancia, Elsa se aísla del mundo, incluyendo de su hermana menor, Anna. Cuando Elsa accidentalmente desata un invierno eterno en Arendelle durante su coronación, huye al monte y se refugia en un palacio de hielo. Anna, con la ayuda de un valiente montañés llamado Kristoff, su reno Sven y un divertido muñeco de nieve llamado Olaf, emprende una aventura épica para encontrar a Elsa y salvar el reino del invierno perpetuo, redescubriendo el vínculo especial que las une como hermanas.",
+        duración: "1h 42m",
+        poster: "./images/Frozen.jpg"
+    },
+
+    {
+        título: "Fragmentado",
+        genero: "Thriller",
+        sinopsis: "Kevin Wendell Crumb es un hombre con trastorno de identidad disociativo, que posee 23 personalidades distintas. Una de sus personalidades, llamada 'La Bestia', empieza a dominar a las otras y lo impulsa a secuestrar a tres adolescentes: Casey, Claire y Marcia. Mientras las chicas intentan escapar, descubren más sobre las diversas personalidades de Kevin, algunas de las cuales intentan ayudarlas y otras que desean mantenerlas cautivas. A medida que 'La Bestia' se manifiesta, Casey debe usar su ingenio y valentía para intentar sobrevivir y enfrentarse a la amenaza que representa Kevin y sus múltiples identidades.",
+        duración: "1h 57m",
+        poster: "./images/Fragmentado.jpg"
+    },
+
+    {
+        título: "Rapido y furiosos",
+        genero: "Acción",
+        sinopsis: "El agente encubierto Brian O'Conner se infiltra en el mundo de las carreras callejeras de Los Ángeles para investigar una serie de robos de camiones. Se gana la confianza de Dominic Toretto, el carismático líder de una pandilla de corredores y sospechoso principal de los robos. Mientras se sumerge más en este mundo lleno de velocidad y adrenalina, Brian se enamora de Mia, la hermana de Dom, y empieza a cuestionar su lealtad. La tensión crece cuando su doble vida lo coloca en un dilema entre cumplir con su deber como policía o salvar a sus nuevos amigos.",
+        duración: "1h 46m",
+        poster: "./images/RapidoYFuriosos.jpg"
+    },
+
+    {
+        título: "Esperando la carroza",
+        genero: "Comedia",
+        sinopsis: "En Buenos Aires, la familia Musicardi se reúne un domingo para almorzar. Mamá Cora, la anciana matriarca, vive con su hijo Jorge y su nuera Susana, quien está agotada por cuidar de ella. Durante el almuerzo, una serie de malentendidos lleva a la familia a creer que Mamá Cora ha desaparecido y posiblemente muerto. Este evento desencadena una serie de situaciones cómicas y revelaciones familiares mientras intentan lidiar con la supuesta muerte de la abuela. La película, una sátira sobre la hipocresía y los conflictos en las familias, es un clásico del cine argentino, conocido por sus diálogos ingeniosos y personajes memorables.",
+        duración: "1h 36m",
+        poster: "./images/EsperandoLaCarroza.jpg"
+    },
+
+    {
+        título: "Querida, encogí a los niños",
+        genero: "Comedia/Familiar",
+        sinopsis: "El excéntrico inventor Wayne Szalinski crea una máquina para encoger objetos, pero su experimento parece ser un fracaso. Todo cambia cuando, accidentalmente, la máquina encoge a sus hijos, Amy y Nick, junto con los niños del vecino, Ron y Russ. Ahora, del tamaño de insectos, los cuatro niños deben enfrentarse a los peligros del jardín trasero, que se ha convertido en un vasto y peligroso terreno. Mientras Wayne y su esposa Diane intentan desesperadamente encontrarlos y revertir el proceso, los niños deben usar su ingenio y trabajar juntos para sobrevivir y volver a su tamaño normal. La película combina aventura, comedia y efectos especiales innovadores, convirtiéndola en un clásico familiar.",
+        duración: "1h 33m",
+        poster: "./images/CHEALN.jpg"
+    },
+
+    {
+        título: "El Secreto De Sus Ojos",
+        genero: "Crimen/Suspenso",
+        sinopsis: "Un juez tiene dudas acerca de los planes de un oficial de justicia recientemente retirado que intenta descubrir al culpable de la violación y el asesinato de una joven, crimen ocurrido varias décadas atrás.",
+        duración: "2h 9m",
+        poster: "./images/ESEDSO.jpg"
+    },
+
+    {
+        título: "Crepusculo",
+        genero: "Romance/Sobrenatural",
+        sinopsis: "Bella Swan se va a vivir con su padre al pequeño pueblo de Forks, donde conoce a Edward, un atractivo y misterioso chico del que se enamora y quien esconde un gran secreto: es un vampiro. Pero la familia del chico guarda una peculiaridad, y es que no se alimenta de sangre humana.",
+        duración: "2h 2m",
+        poster: "./images/Crepusculo.jpg"
+    },
+
+    {
+        título: "El diablo viste a la moda",
+        genero: "Comedia",
+        sinopsis: "El mundo de la moda está gobernado por la déspota y sofisticada Miranda Priestly. Trabajar como su ayudante podría abrirle cualquier puerta a la inocente Andy Sachs, pero para ello tendrá que encajar entre las periodistas de la revista Runway.",
+        duración: "1h 49m",
+        poster: "./images/EDVP.jpg"
+    },
+
+    {
+        título: "Scream",
+        genero: "Terror/Misterio",
+        sinopsis: "Un asesino en serie, que viste una máscara blanca y una túnica negra, siembra el pánico entre los adolescentes de un pueblo californiano.",
+        duración: "1h 51m",
+        poster: "./images/Scream.jpg"
+    },
+
+    {
+        título: "Los Simpsons",
+        genero: "Comedia/Familiar",
+        sinopsis: "El día a día de la familia Simpsons en Springfield, una ciudad de los Estados Unidos",
+        temporadas: 35,
+        duración: "24 min",
+        poster: "./images/TheSimpsons.jfif"
+    },
+
+    {
+        título: "El Maravilloso Mundo de Gumball",
+        genero: "Comedia/Familiar",
+        sinopsis: "Las aventuras de Gumball, un gato azul, junto con su familia en la ciudad de elmore",
+        temporadas: 6,
+        duración: "24 min",
+        poster: "./images/EIMDG.jpg"
+    },
+
+    {
+        título: "Fallout",
+        genero: "Acción/Drama",
+        sinopsis: "En un futuro posapocalíptico provocado por la devastación nuclear, los ciudadanos de Los Ángeles deben vivir en búnkeres subterráneos para protegerse de la radiación, los mutantes y los bandidos.",
+        temporadas: 1,
+        duración: "1h",
+        poster: "./images/Fallout.jpg"
+    },
+
+    {
+        título: "Brooklyn99",
+        genero: "Comedia",
+        sinopsis: "Los detectives Jake Peralta, Amy Santiago, Rosa Diaz, Charles Boyle y el sargento Terry Jeffords forman una simpática y poco convencional brigada, pero todo cambia tras la llegada del nuevo jefe, el inflexible Raymond Holt.",
+        temporadas: 8,
+        duración: "23 min",
+        poster: "./images/Brooklyn99.jpg"
+    },
+
+    {
+        título: "Hora de Aventura",
+        genero: "Comedia/Fantasía",
+        sinopsis: "narra las extravagantes, coloridas y a menudo absurdas y divertidas aventuras de un niño llamado Finn y su amigo canino parlante, Jake , que se unen para divertirse en la mística Tierra de Ooo.",
+        temporadas: 10,
+        duración: "15 min",
+        poster: "./images/AdventureTime.jpg"
+    },
+
+    {
+        título: "Fullmetal Alchemist",
+        genero: "Acción/Fantasía",
+        sinopsis: "dos hermanos que intentaron resucitar a su madre muerta usando una ciencia muy antigua y secreta de la alquimia. Pero el ritual sale mal y el cuerpo de uno de ello se desmaterializa y su mente queda atrapada para siempre en una armadura de metal.",
+        temporadas: 1,
+        duración: "24 min",
+        poster: "./images/FullMetalAlchemist.jpg"
+    },
+
+    {
+        título: "Gossip Girl",
+        genero: "Drama/Romance",
+        sinopsis: " se basa en Gossip Girl, el cual es un blog en Internet muy conocido, en donde se narra día a día chismes, conflictos amorosos, familiares y económicos de un grupo de jóvenes multimillonarios de la elite de Manhattan.",
+        temporadas: 6,
+        duración: "42 min",
+        poster: "./images/GossipGirls.jpg"
+    },
+
+    {
+        título: "Gambito De Dama",
+        genero: "Drama",
+        sinopsis: "En un orfanato de los años 50, una chica exhibe un talento extraordinario para el ajedrez. A medida que su fama sube como la espuma, intenta superar su adicción.",
+        temporadas: 1,
+        duración: "46 min / 67 min",
+        poster: "./images/GambitoDeDamas.jpg"
+    },
+
+    {
+        título: "Phineas y Ferb",
+        genero: "Comedia/Familiar",
+        sinopsis: "Phineas y Ferb son dos hermanastros que quieren disfrutar al máximo de las vacaciones de verano. No dejan de hacer trastadas y de inventar instrumentos imposibles como un monopatín gigante para sufrimiento de su hermana mayor Candace, que no les aguanta.",
+        temporadas: 4,
+        duración: "22 min",
+        poster: "./images/FineasYFerb.jfif"
+    },
+]
+
+function agregarSeriesYPeliculas(){
+    
+}
