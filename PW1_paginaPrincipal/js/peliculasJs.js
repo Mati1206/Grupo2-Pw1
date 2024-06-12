56ac4ce20b99c840d6561bdcb639337f9cf4359e
MateoColace <166334360+MateoColace@users.noreply.github.com>
2024-06-12 19:52:37 -0300

    Delete peliculasJs.js

--- PW1_paginaPrincipal/js/peliculasJs.js
+++
@@ -1,7 +0,0 @@
-const ARRAY_PELICULAS = []
-
-for(let i = 0; i < PELICULAS_Y_SERIES.length; i++){
-    if(PELICULAS_Y_SERIES[i].categoria === 'pelicula'){
-        ARRAY_PELICULAS[i] = PELICULAS_Y_SERIES[i]
-    }
-}
