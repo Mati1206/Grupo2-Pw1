1baee230bda7f5289a4e078b85b8eaa3b2eaeeda
MateoColace <166334360+MateoColace@users.noreply.github.com>
2024-06-17 01:46:47 -0300

    agregue una sección de salida

---
+++ PW1_paginaPrincipal/js/avisoJS.js
@@ -0,0 +1,6 @@
+const aviso = document.getElementById('aviso')
+const nodo_h1 = document.createElement('h1')
+const adv = document.createTextNode("Advertencia")
+
+nodo_h1.appendChild(adv)
+aviso.appendChild(nodo_h1)
--- PW1_paginaPrincipal/js/indexJs.js
+++ PW1_paginaPrincipal/js/indexJs.js
@@ -31,15 +31,7 @@ function mostrarSeries(){
     })
 }
 
-function mostrarAviso(){
-    const nodo_salir = document.getElementById('cerrarSesion')
-    nodo_salir.addEventListener('click', (event) => {
-        event.preventDefault()
-        alert("Al salir, los datos guardados se pueden perder")
-    })
-}
 
-mostrarAviso()
 mostrarSeries()
 mostrarPeliculas()
 mostrarPeliculasYSeries()
