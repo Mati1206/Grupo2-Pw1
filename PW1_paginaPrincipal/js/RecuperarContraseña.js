dafd5a0603f7d6258cc4c7ad47365619de54b356
Mati1206 <mattiasaragon@gmail.com>
2024-06-19 01:59:38 -0300

    agrego validacion en recuperar contraseña

--- PW1_paginaPrincipal/js/RecuperarContraseña.js
+++ PW1_paginaPrincipal/js/RecuperarContraseña.js
@@ -1,38 +1,16 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const emailInput = document.getElementById('email');
-    const usuarioInput = document.getElementById('usuario');
-    const submitButton = document.getElementById('button');
+document.getElementById('campo').addEventListener('submit', function(event) {
+    const error=document.getElementById('error')
+    event.preventDefault(); 
+    const email = document.getElementById('email').value;
+    const usuario = document.getElementById('usuario').value;
+    const usuarios = JSON.parse(localStorage.getItem('usuarios')) || [];
+    const usuarioEncontrado = usuarios.find(buscador => buscador.email === email && buscador.usuario === usuario);
 
-    // validar formulario
-    function validateForm() {
-        const email = emailInput.value.trim();
-        const usuario = usuarioInput.value.trim();
-        
-        if (email === '' || usuario === '') {
-            return false;
-        }
-
-        return true;
+    if (usuarioEncontrado) {
+        error.textContent=""
+       window.location.href='login.html'
+    } else {
+      error.textContent="Email o nombre de usuario incorrecto"
+      error.style.color="red"
     }
-
-    // deshabilitar el botón de enviar
-    function toggleSubmitButton() {
-        if (validateForm()) {
-            submitButton.disabled = false;
-        } else {
-            submitButton.disabled = true;
-        }
-    }
-
-    // eventos de entrada a los campos de formulario
-    emailInput.addEventListener('input', toggleSubmitButton);
-    usuarioInput.addEventListener('input', toggleSubmitButton);
-
-    // validar formulario para enviar
-    document.querySelector('form').addEventListener('submit', (e) => {
-        if (!validateForm()) {
-            e.preventDefault();
-            alert('Por favor, complete todos los campos.');
-        }
-    });
 });
