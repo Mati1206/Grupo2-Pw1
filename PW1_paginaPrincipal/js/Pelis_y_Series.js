993bd1d177254527663babd18d910bfdfcd28395
MateoColace <166334360+MateoColace@users.noreply.github.com>
2024-06-23 20:33:32 -0300

    .

--- PW1_paginaPrincipal/js/Pelis_y_Series.js
+++ PW1_paginaPrincipal/js/Pelis_y_Series.js
@@ -6,7 +6,8 @@ const PELICULAS_Y_SERIES =
         sinopsis: "John Wick, un exasesino a sueldo, vive en paz tras la muerte de su esposa. Su tranquilidad se quiebra cuando el hijo de un mafioso roba su auto y mata a su cachorro, último regalo de su esposa. Esto lo lleva a retomar su antigua vida y buscar venganza contra quienes le arrebataron lo que más amaba.",
         duración: "1h 41m",
         poster: "./images/JohnWick.jpg",
-        categoria: 1
+        categoria: 1,
+        video: "https://www.youtube.com/embed/C0BMx-qxsP4?si=bsgR64-_lG5H8OrZ"
     },
 
     {
@@ -16,7 +17,8 @@ const PELICULAS_Y_SERIES =
         temporadas: 1,
         duración: "46 min / 67 min",
         poster: "./images/GambitoDeDamas.jpg",
-        categoria: 2
+        categoria: 2,
+        video: "https://www.youtube.com/embed/oZn3qSgmLqI?si=EqAOiIVVc-ft2Qu5"
     },
 
     {
@@ -25,7 +27,8 @@ const PELICULAS_Y_SERIES =
         sinopsis: "En el reino de Arendelle, la joven princesa Elsa posee poderes mágicos que le permiten crear hielo y nieve. Tras un accidente durante su infancia, Elsa se aísla del mundo, incluyendo de su hermana menor, Anna. Cuando Elsa accidentalmente desata un invierno eterno en Arendelle durante su coronación, huye al monte y se refugia en un palacio de hielo. Anna, con la ayuda de un valiente montañés llamado Kristoff, su reno Sven y un divertido muñeco de nieve llamado Olaf, emprende una aventura épica para encontrar a Elsa y salvar el reino del invierno perpetuo, redescubriendo el vínculo especial que las une como hermanas.",
         duración: "1h 42m",
         poster: "./images/Frozen.jpg",
-        categoria: 1
+        categoria: 1,
+        video: "https://www.youtube.com/embed/SRRRrCNTVJA?si=c_-llSDBu0WNJ0oS"
     },
 
     {
@@ -35,7 +38,8 @@ const PELICULAS_Y_SERIES =
         temporadas: 6,
         duración: "24 min",
         poster: "./images/EIMDG.jpg",
-        categoria: 2
+        categoria: 2,
+        video: "https://www.youtube.com/embed/LOBOJQocArs?si=XPtnPMyx8B3Qsbnm"
     },
 
     {
@@ -44,7 +48,8 @@ const PELICULAS_Y_SERIES =
         sinopsis: "El agente encubierto Brian O'Conner se infiltra en el mundo de las carreras callejeras de Los Ángeles para investigar una serie de robos de camiones. Se gana la confianza de Dominic Toretto, el carismático líder de una pandilla de corredores y sospechoso principal de los robos. Mientras se sumerge más en este mundo lleno de velocidad y adrenalina, Brian se enamora de Mia, la hermana de Dom, y empieza a cuestionar su lealtad. La tensión crece cuando su doble vida lo coloca en un dilema entre cumplir con su deber como policía o salvar a sus nuevos amigos.",
         duración: "1h 46m",
         poster: "./images/RapidoYFuriosos.jpg",
-        categoria: 1
+        categoria: 1,
+        video: "https://www.youtube.com/embed/2TAOizOnNPo?si=N9LF6HGsm-ap2hRO"
     },
 
     {
@@ -53,7 +58,8 @@ const PELICULAS_Y_SERIES =
         sinopsis: "En Buenos Aires, la familia Musicardi se reúne un domingo para almorzar. Mamá Cora, la anciana matriarca, vive con su hijo Jorge y su nuera Susana, quien está agotada por cuidar de ella. Durante el almuerzo, una serie de malentendidos lleva a la familia a creer que Mamá Cora ha desaparecido y posiblemente muerto. Este evento desencadena una serie de situaciones cómicas y revelaciones familiares mientras intentan lidiar con la supuesta muerte de la abuela. La película, una sátira sobre la hipocresía y los conflictos en las familias, es un clásico del cine argentino, conocido por sus diálogos ingeniosos y personajes memorables.",
         duración: "1h 36m",
         poster: "./images/EsperandoLaCarroza.jpg",
-        categoria: 1
+        categoria: 1,
+        video: "https://www.youtube.com/embed/KIgOuEPQZsU?si=Rg5Zao5PX_uv-VND"
     },
 
     {
@@ -62,7 +68,8 @@ const PELICULAS_Y_SERIES =
         sinopsis: "El excéntrico inventor Wayne Szalinski crea una máquina para encoger objetos, pero su experimento parece ser un fracaso. Todo cambia cuando, accidentalmente, la máquina encoge a sus hijos, Amy y Nick, junto con los niños del vecino, Ron y Russ. Ahora, del tamaño de insectos, los cuatro niños deben enfrentarse a los peligros del jardín trasero, que se ha convertido en un vasto y peligroso terreno. Mientras Wayne y su esposa Diane intentan desesperadamente encontrarlos y revertir el proceso, los niños deben usar su ingenio y trabajar juntos para sobrevivir y volver a su tamaño normal. La película combina aventura, comedia y efectos especiales innovadores, convirtiéndola en un clásico familiar.",
         duración: "1h 33m",
         poster: "./images/CHEALN.jpg",
-        categoria: 1
+        categoria: 1,
+        video: "https://www.youtube.com/embed/cN19AazIWLk?si=W7HeqTKvIXB12bhA"
     },
 
     {
@@ -71,7 +78,8 @@ const PELICULAS_Y_SERIES =
         sinopsis: "Un juez tiene dudas acerca de los planes de un oficial de justicia recientemente retirado que intenta descubrir al culpable de la violación y el asesinato de una joven, crimen ocurrido varias décadas atrás.",
         duración: "2h 9m",
         poster: "./images/ESEDSO.jpg",
-        categoria: 1
+        categoria: 1,
+        video: "https://www.youtube.com/embed/hKa8U-8vsfU?si=NphNMGpLInoT9FdU"
     },
 
     {
@@ -80,7 +88,8 @@ const PELICULAS_Y_SERIES =
         sinopsis: "Bella Swan se va a vivir con su padre al pequeño pueblo de Forks, donde conoce a Edward, un atractivo y misterioso chico del que se enamora y quien esconde un gran secreto: es un vampiro. Pero la familia del chico guarda una peculiaridad, y es que no se alimenta de sangre humana.",
         duración: "2h 2m",
         poster: "./images/Crepusculo.jpg",
-        categoria: 1
+        categoria: 1,
+        video: "https://www.youtube.com/embed/kX2DKZcDM-o?si=xcXti2L_Y7iswl0a"
     },
 
     {
@@ -89,7 +98,8 @@ const PELICULAS_Y_SERIES =
         sinopsis: "El mundo de la moda está gobernado por la déspota y sofisticada Miranda Priestly. Trabajar como su ayudante podría abrirle cualquier puerta a la inocente Andy Sachs, pero para ello tendrá que encajar entre las periodistas de la revista Runway.",
         duración: "1h 49m",
         poster: "./images/EDVP.jpg",
-        categoria: 1
+        categoria: 1,
+        video: "https://www.youtube.com/embed/6ZOZwUQKu3E?si=jsrNN_3lTUGcKWg4"
     },
 
     {
@@ -98,7 +108,8 @@ const PELICULAS_Y_SERIES =
         sinopsis: "Un asesino en serie, que viste una máscara blanca y una túnica negra, siembra el pánico entre los adolescentes de un pueblo californiano.",
         duración: "1h 51m",
         poster: "./images/Scream.jpg",
-        categoria: 1
+        categoria: 1,
+        video: "https://www.youtube.com/embed/i3J6ACKQ7K0?si=0gTzIXQ4qUMZ6KPj"
     },
 
     {
@@ -108,7 +119,8 @@ const PELICULAS_Y_SERIES =
         temporadas: 35,
         duración: "24 min",
         poster: "./images/TheSimpsons.jfif",
-        categoria: 2
+        categoria: 2,
+        video: "https://www.youtube.com/embed/aDcFhYtiIEM?si=gF9PYibgIdn8Utuc"
     },
 
     {
@@ -117,7 +129,8 @@ const PELICULAS_Y_SERIES =
         sinopsis: "Kevin Wendell Crumb es un hombre con trastorno de identidad disociativo, que posee 23 personalidades distintas. Una de sus personalidades, llamada 'La Bestia', empieza a dominar a las otras y lo impulsa a secuestrar a tres adolescentes: Casey, Claire y Marcia. Mientras las chicas intentan escapar, descubren más sobre las diversas personalidades de Kevin, algunas de las cuales intentan ayudarlas y otras que desean mantenerlas cautivas. A medida que 'La Bestia' se manifiesta, Casey debe usar su ingenio y valentía para intentar sobrevivir y enfrentarse a la amenaza que representa Kevin y sus múltiples identidades.",
         duración: "1h 57m",
         poster: "./images/Fragmentado.jpg",
-        categoria: 1
+        categoria: 1,
+        video: "https://www.youtube.com/embed/KIpGKumxiGg?si=QNOKjRP3RcS5Blxa"
     },
 
     {
@@ -127,7 +140,8 @@ const PELICULAS_Y_SERIES =
         temporadas: 1,
         duración: "1h",
         poster: "./images/Fallout.jpg",
-        categoria: 2
+        categoria: 2,
+        video: "https://www.youtube.com/embed/GExflKHU3VE?si=ubt77dSeov-D2rl4"
     },
 
     {
@@ -137,7 +151,8 @@ const PELICULAS_Y_SERIES =
         temporadas: 8,
         duración: "23 min",
         poster: "./images/Brooklyn99.jpg",
-        categoria: 2
+        categoria: 2,
+        video: "https://www.youtube.com/embed/HlBYdiXdUa8?si=8DVHBO1twslRpP6d"
     },
 
     {
@@ -148,6 +163,7 @@ const PELICULAS_Y_SERIES =
         duración: "15 min",
         poster: "./images/AdventureTime.jpg",
         categoria: 2,
+        video: "https://www.youtube.com/embed/kMDaAcfCYow?si=Bj99dPEC_sHQG5bC"
     },
 
     {
@@ -158,6 +174,7 @@ const PELICULAS_Y_SERIES =
         duración: "24 min",
         poster: "./images/FullMetalAlchemist.jpg",
         categoria: 2,
+        video: "https://www.youtube.com/embed/QJHaXGhBBbc?si=PiEN7ta74sSJnyFA"
     },
 
     {
@@ -168,6 +185,7 @@ const PELICULAS_Y_SERIES =
         duración: "42 min",
         poster: "./images/GossipGirls.jpg",
         categoria: 2,
+        video: "https://www.youtube.com/embed/XhsV1gPVJMQ?si=V85rAOKlw5pwcAtW"
     },
 
     {
@@ -177,6 +195,7 @@ const PELICULAS_Y_SERIES =
         duración: "2h",
         poster: "./images/TheLoveWithc.jpg",
         categoria: 1,
+        video: "https://www.youtube.com/embed/BHhaIRevB-Y?si=ZH4vHRXuqKGnh-h2"
     },
 
     {
@@ -187,8 +206,10 @@ const PELICULAS_Y_SERIES =
         duración: "22 min",
         poster: "./images/FineasYFerb.jfif",
         categoria: 2,
+        video: "https://www.youtube.com/embed/Nt5AuDn6BwE?si=e0pRsM5KlUk1fMh1"
     }
 ]
+
 
 function actualizarCatalogo(catalogo){
     const nodo_pelicualasYseries = document.querySelector('.peliculasYSeries')
@@ -211,14 +232,8 @@ function actualizarCatalogo(catalogo){
 
         let nodo_a = document.createElement('a')
 
-        if(catalogo[i].categoria === 1){
-             nodo_a.href = "./detallePelicula.html"
-        }else{
-            if(catalogo[i].categoria === 2){
-             nodo_a.href = "./detalleSerie.html"
-            }
-        }
-       
+        nodo_a.href = `./detalleSerieOPelicula.html?id=${i}`
+
         nodo_a.appendChild(nodo_imagen)
         nodo_article.appendChild(nodo_a)
         nodo_linea.appendChild(nodo_article)
--- PW1_paginaPrincipal/js/detallePeliculaOSerie.js
+++ PW1_paginaPrincipal/js/detallePeliculaOSerie.js
@@ -27,4 +27,44 @@ const flkty = new Flickity(carousel, {
   autoPlay: 1500,
 });
 
+function obtenerParametrosUrl(name){
+  const parametrosUrl = new URLSearchParams(window.location.search)
+  return parametrosUrl.get(name)
+}
 
+const id = obtenerParametrosUrl('id')
+const item = PELICULAS_Y_SERIES[id]
+
+function generarDetalleSeriePelicula(item) {
+  return `
+      <article class="foto">
+          <div class="imagen">
+              <iframe width="400em" height="250em"
+                  src="${item.video}"
+                  title="YouTube video player" frameborder="0"
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                  referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+          </div>
+          <div class="boton">
+              <a target="_blank" href="${item.video}">
+                  <button class="comenzar">Comenzar</button>
+              </a>
+          </div>
+      </article>
+      <article class="descripcion">
+          <h2>${item.título}</h2>
+          <p><strong>Género:</strong> ${item.genero}</p>
+          <p><strong>Duración:</strong> ${item.duración}</p>
+          <p><strong>Sinopsis:</strong> ${item.sinopsis}</p>
+      </article>
+  `
+}
+
+function mostrarInformacionPeliculaSerie(){
+ const container = document.getElementById('contenedor')
+ if(item){
+    container.innerHTML = generarDetalleSeriePelicula(item)
+ }
+}
+
+mostrarInformacionPeliculaSerie()
