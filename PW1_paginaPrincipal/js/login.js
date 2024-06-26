84a4cf77dc5272c1318828f49ee53e852aa01725
JoanDelBarrio <160414073+JoanDelBarrio@users.noreply.github.com>
2024-06-26 15:01:17 -0300

    terminado 2.0

--- PW1_paginaPrincipal/js/login.js
+++ PW1_paginaPrincipal/js/login.js
@@ -1,17 +1,24 @@
 document.getElementById('campo').addEventListener('submit', function(event) {
-    const error= document.getElementById("error")
     event.preventDefault(); 
-    const usuario = document.getElementById('Usuario').value;
-    const contraseña = document.getElementById('contraseña').value;
+    const usuario = document.getElementById('Usuario').value.trim();
+    const contraseña = document.getElementById('contraseña').value.trim();
+    const error = document.getElementById("error");
+
+    if (!usuario || !contraseña) {
+        error.textContent = "Ambos campos son obligatorios";
+        error.style.color = "red";
+        return;
+    }
+    
     const usuarios = JSON.parse(localStorage.getItem('usuarios')) || [];
     const usuarioEncontrado = usuarios.find(buscador => buscador.usuario === usuario && buscador.contraseña === contraseña);
 
     if (usuarioEncontrado) {
         localStorage.setItem('usuarioLogueado', JSON.stringify(usuarioEncontrado));
-        error.textContent=""
+        error.textContent = "";
         window.location.href = 'home.html';
     } else {
-        error.textContent="Usuario o contraseña incorrecto"
-        error.style.color="red"
+        error.textContent = "Usuario o contraseña incorrecto";
+        error.style.color = "red";
     }
 });
